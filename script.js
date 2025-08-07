fdaa3d40ff95202ede17afefd177a7f57b60001d
mobinmiah <mobinmiah1990@gmail.com>
2025-08-08 05:31:16 +0600

    responsive menu done

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-let bar = document.getElementById(bar - icon);
-let menu = document.getElementById(menuList);
-let close = document.getElementById(x - icon);
+let bar = document.getElementById('bar-icon');
+let menu = document.getElementById('menuList');
+let close = document.getElementById('x-icon');
+
+bar.addEventListener('click', () => {
+    menu.style.display = 'block';
+    close.style.display = 'block';
+    bar.style.display= 'none'
+});
+
+close.addEventListener('click', () => {
+    menu.style.display = 'none';
+    close.style.display = 'none';
+    bar.style.display= 'block'
+});
+
+
+
+
