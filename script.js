dfa023f384e6b49fb087de9e71ce617ce39808a9
mobinmiah <mobinmiah1990@gmail.com>
2025-08-25 03:42:35 +0600

    toggle done

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-let bar = document.getElementById('bar-icon');
-let menu = document.getElementById('menuList');
-let close = document.getElementById('x-icon');
-
-bar.addEventListener('click', () => {
-    menu.style.display = 'block';
-    close.style.display = 'block';
-    bar.style.display= 'none'
-});
-
-close.addEventListener('click', () => {
-    menu.style.display = 'none';
-    close.style.display = 'none';
-    bar.style.display= 'block'
-});
-
-
+document.getElementById('bar-icon')
+    .addEventListener('click', function () {
+        document.getElementById('menuList').style.display = 'block';
+        const close = document.getElementById('x-icon');
+        close.style.display = 'block';
+        if (close.style.display === 'block') {
+            document.getElementById('bar-icon').style.display = 'none';
+        }
+    });
+document.getElementById('x-icon')
+    .addEventListener('click', function () {
+        document.getElementById('menuList').style.display = 'none';
+        const bar = document.getElementById('bar-icon');
+        bar.style.display = 'block';
+        if (bar.style.display === 'block') {
+            document.getElementById('x-icon').style.display = 'none';
+        }
+    })
 
 
